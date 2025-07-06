93cf87993e5443e41db15aa83896f334fbe4e4c5
Mael <mael.liang@live.com>
2025-07-06 10:44:59 +0800

    feat: 在认证服务器中添加文件头注释，优化代码格式，更新信任主机配置

--- src/auth/server.ts
+++ src/auth/server.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: Mael mael.liang@live.com
+ * @Date: 2025-07-05 16:15:05
+ * @LastEditors: Mael mael.liang@live.com
+ * @LastEditTime: 2025-07-06 10:44:41
+ * @FilePath: /exif-photo-blog/src/auth/server.ts
+ * @Description:
+ */
 import { isPathProtected } from '@/app/paths';
 import NextAuth, { User } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
@@ -12,8 +20,10 @@ export const {
     Credentials({
       async authorize({ email, password }) {
         if (
-          process.env.ADMIN_EMAIL && process.env.ADMIN_EMAIL === email &&
-          process.env.ADMIN_PASSWORD && process.env.ADMIN_PASSWORD === password
+          process.env.ADMIN_EMAIL &&
+          process.env.ADMIN_EMAIL === email &&
+          process.env.ADMIN_PASSWORD &&
+          process.env.ADMIN_PASSWORD === password
         ) {
           const user: User = { email, name: 'Admin User' };
           return user;
@@ -37,11 +47,10 @@ export const {
   pages: {
     signIn: '/sign-in',
   },
+  trustHost: true,
 });
 
-export const runAuthenticatedAdminServerAction = async <T>(
-  callback: () => T,
-): Promise<T> => {
+export const runAuthenticatedAdminServerAction = async <T>(callback: () => T): Promise<T> => {
   const session = await auth();
   if (session?.user) {
     return callback();
